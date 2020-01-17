687dcd096c8857a17fe0a0346e7830b410a648ca
Carlos <carlos.bergamasco@gmail.com>
2020-01-17 16:42:39 -0300

    Exemplo regex do livro

---
+++ livro_automatizando/regex.py
@@ -0,0 +1,21 @@
+import re
+
+batRegex = re.compile(r'Bat(wo)+man')
+mo1 = batRegex.search('The Adventures of Batwoman')
+print(mo1.group())
+mo2 = batRegex.search('The Adventures of Batwowowowoman')
+print(mo2.group())
+mo3 = batRegex.search('The Adventures of Batman')
+print(mo3 == None)
+
+haRegex = re.compile(r'(Ha){3}')
+mo1 = haRegex.search('HaHaHa')
+print(mo1.group())
+
+
+greedyHaRegex = re.compile(r'(Ha){3,5}')
+mo1 = greedyHaRegex.search('HaHaHaHaHa')
+print(mo1.group())
+nongreedyHaRegex = re.compile(r'(Ha){3,5}?')
+mo2 = nongreedyHaRegex.search('HaHaHaHaHa')
+print(mo2.group())
