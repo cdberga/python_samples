782f1fedfa8c6703eb0b6ebcecde30d09dc2eb52
Carlos <carlos.bergamasco@gmail.com>
2020-01-22 18:21:38 -0300

    More regex

--- livro_automatizando/regex.py
+++ livro_automatizando/regex.py
@@ -22,3 +22,37 @@ print(mo2.group())
 
 phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d') # não tem nenhum grupo
 print(phoneNumRegex.findall('Cell: 415-555-9999 Work: 212-555-0000'))
+
+""" Classe de caracteres        Representa
+\d                              Qualquer dígito de 0 a 9.
+\D                              Qualquer caractere que não seja um dígito de 0 a 9.
+\w                              Qualquer letra, dígito ou o caractere underscore. (Pense nisso como uma correspondência de caracteres de “palavra”.)
+\W                              Qualquer caractere que não seja uma letra, um dígito ou o caractere underscore.
+\s                              Qualquer espaço, tabulação ou caractere de quebra de linha. (Pense nisso como uma correspondência de caracteres de “espaço”.)
+\S                              Qualquer caractere que não seja um espaço, uma tabulação ou uma quebra de linha. """
+
+xmasRegex = re.compile(r'\d+\s\w+')
+print(xmasRegex.findall('12 drummers, 11 pipers, 10 lords, 9 ladies, 8 maids, 7 swans, 6 geese, 5 rings, 4 birds, 3 hens, 2 doves, 1 partridge'))
+
+consonantRegex = re.compile(r'[^aeiouAEIOU]')
+print(consonantRegex.findall('RoboCop eats baby food. BABY FOOD.'))
+
+# Non greedy, faz a correspondencia com a menor string possível
+nongreedyRegex = re.compile(r'<.*?>')
+mo = nongreedyRegex.search('<To serve man> for dinner.>')
+print(mo.group())
+
+# Greedy, faz a correspondencia com a maior string possível
+greedyRegex = re.compile(r'<.*>')
+mo = greedyRegex.search('<To serve man> for dinner.>')
+print(mo.group())
+
+newlineRegex = re.compile('.*', re.DOTALL)
+print(newlineRegex.search('Serve the public trust.\nProtect the innocent.\nUphold the law.').group())
+
+namesRegex = re.compile(r'Agent \w+')
+print(namesRegex.sub('CENSORED', 'Agent Alice gave the secret documents to Agent Bob.'))
+
+agentNamesRegex = re.compile(r'Agent (\w)\w*')
+print(agentNamesRegex.sub(r'\1****', 'Agent Alice told Agent Carol that Agent Eve knew Agent Bob was a double agent.'))
+
