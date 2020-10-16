e13ba58f1185d6b5b15a1c4716eedac4d7f5eee7
Carlos <carlos.bergamasco@gmail.com>
2020-10-16 20:13:51 -0300

    Add Equalize array HR challenge

--- exemplos/equalize_array.py
+++ exemplos/equalize_array.py
@@ -8,9 +8,25 @@ import sys
 
 # Complete the equalizeArray function below.
 def equalizeArray(arr):
-    # analizar o array para encontrar itens repetidos
-    # dos itens repetidos ver quais tem maior ocorrencia
-    # escolhido o item de maior ocorrencia fazer a contagem necessaria para remover os demais 
+    item_map = {}
+    for item in arr:
+        if item_map.get(item) == None:
+            item_map[item] = 1
+        else:
+            item_map[item] += 1
+
+    return get_deletes(item_map)
+
+def get_deletes(the_map):
+    max_occur = 0
+    sum = 0
+    for occurs in the_map.values():
+        sum += occurs
+        if occurs > max_occur:
+            max_occur = occurs
+
+    return sum - max_occur
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
